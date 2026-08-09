4181570a22cbea6ec84f53c2fcc5ed1987aa1893
Daniel Saghaleini <dsaghaleini@icloud.com>
2026-08-09 15:52:15 -0700

    shooting image added when shooting is happening =)

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ RIGHT_BIN_IMAGE = pygame.image.load("bin_2.png")
 SHOOTING_LEFT_BIN_IMAGE = pygame.image.load("shooting_left_bin.png")
 SHOOTING_RIGHT_BIN_IMAGE = pygame.image.load("shooting_right_bin.png")
 SHOOTING_BIN_IMAGE = pygame.image.load("shooting_bin.png")
+LEFT_BIN_BALLIN_IMAGE = pygame.image.load("BIN_BALLIN!.png")
+RIGHT_BIN_BALLIN_IMAGE = pygame.image.load("BIN_BALLIN 2!.png")
 
 CURRENT_BIN_IMAGE = LEFT_BIN_IMAGE
 
@@ -30,6 +32,7 @@ running = True
 
 bin_x = SCREEN_WIDTH/2
 bin_y = SCREEN_HEIGHT * .3
+bin_shooting = 0
 starting_bin_y = bin_y
 highest_bin_y = bin_y
 camera_y = 0
@@ -91,7 +94,7 @@ class Platform:
         global hidden_platforms
         if self.active:
             if game_y_to_screen(self.y) > SCREEN_HEIGHT:
-                if hidden_platforms < platforms_to_hide and random.randint(1,10) == 1:
+                if hidden_platforms < platforms_to_hide and random.randint(1,5) == 1:
                     hidden_platforms += 1
                     self.active = False
                 else:
@@ -143,6 +146,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # shooting
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            bin_shooting = 10
 
     # score
     score = camera_y/3
@@ -152,8 +158,10 @@ while running:
     if bin_y > highest_bin_y:
         highest_bin_y = bin_y
     camera_y = highest_bin_y - starting_bin_y
-
     bin_y_speed = bin_y_speed - 0.07
+
+    # timers
+    bin_shooting -= 1
 
     for platform in platforms:
         platform.bounce_player()
@@ -165,15 +173,26 @@ while running:
     if bin_x < -BIN_WIDTH/2:
         bin_x = SCREEN_WIDTH + BIN_WIDTH/2
 
-    # move left and right
+    # moving
     pressed_keys = pygame.key.get_pressed()
-    if pressed_keys[pygame.K_LEFT]:
-        bin_x = bin_x - 3
-        CURRENT_BIN_IMAGE = LEFT_BIN_IMAGE
-    if pressed_keys[pygame.K_RIGHT]:
-        bin_x = bin_x + 3
-        CURRENT_BIN_IMAGE = RIGHT_BIN_IMAGE
-        
+    if score < 6500:
+        if pressed_keys[pygame.K_LEFT]:
+            bin_x = bin_x - 3
+            CURRENT_BIN_IMAGE = LEFT_BIN_IMAGE
+        if pressed_keys[pygame.K_RIGHT]:
+            bin_x = bin_x + 3
+            CURRENT_BIN_IMAGE = RIGHT_BIN_IMAGE
+    else:
+        if pressed_keys[pygame.K_LEFT]:
+            bin_x = bin_x - 3
+            CURRENT_BIN_IMAGE = LEFT_BIN_BALLIN_IMAGE
+        if pressed_keys[pygame.K_RIGHT]:
+            bin_x = bin_x + 3
+            CURRENT_BIN_IMAGE = RIGHT_BIN_BALLIN_IMAGE
+
+    if bin_shooting > 0:
+        CURRENT_BIN_IMAGE = SHOOTING_BIN_IMAGE
+    
     # losing
     if game_y_to_screen(bin_y) >= SCREEN_HEIGHT + BIN_HEIGHT:
         BG_COLOR = ("#FFDCDC")
@@ -188,9 +207,11 @@ while running:
     SCORE_IMAGE = SCORE_FONT.render(str(int(score)), True, "black")
     screen.blit(SCORE_IMAGE, (10, 10))
 
-    # remove a platform when appropriate
+    # changing overtime
     if score >= 300:
-        platforms_to_hide = 5
+        platforms_to_hide = 2
+    if score >= 600:
+        platforms_to_hide = 4
 
     # flip() the display to put your work on screen
     pygame.display.flip()
